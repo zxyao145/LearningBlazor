0d2aaca7c22ddeb1a633b5c39ee3aeb0675f30ff
zxyao <zxyao145@gmail.com>
2020-04-05 12:15:43 +0800

    BlazorCrossComponentInterop

---
+++ BlazorCrossComponentInterop/EventDispatcher.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorCrossComponentInterop
+{
+    public static class EventDispatcher
+    {
+        private static Dictionary<string, Action<object>> _actions;
+        static EventDispatcher()
+        {
+            _actions = new Dictionary<string, Action<object>>();
+        }
+
+        public static void AddAction(string key, Action<object> action)
+        {
+            if (!_actions.ContainsKey(key))
+            {
+                _actions.Add(key, action);
+            }
+            else
+            {
+                throw new Exception($"event key{key} has existed");
+            }
+        }
+
+        public static void RemoveAction(string key)
+        {
+            if (_actions.ContainsKey(key))
+            {
+                _actions.Remove(key);
+            }
+        }
+
+        public static void Dispatch(string key, object value)
+        {
+            Console.WriteLine("Dispatch");
+            Console.WriteLine(string.Join(",", _actions.Keys));
+            if (_actions.ContainsKey(key))
+            {
+                var act = _actions[key];
+                act.Invoke(value);
+            }
+
+        }
+    }
+}
---
+++ BlazorCrossComponentInterop/Models/CascadingModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorCrossComponentInterop
+{
+    public class CascadingModel<T>
+    {
+        public CascadingModel()
+        {
+            
+        }
+
+        public CascadingModel(T defaultValue)
+        {
+            _value = defaultValue;
+        }
+        public Action StateHasChanged;
+
+        private T _value;
+        public T Value
+        {
+            get => _value;
+            set
+            {
+                _value = value;
+                StateHasChanged?.Invoke();
+            }
+        }
+    }
+}
